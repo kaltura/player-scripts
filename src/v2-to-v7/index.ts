c55cccc31ed341b4378cf2099953d421ba0b3ebf
lianbenjamin <79077248+lianbenjamin@users.noreply.github.com>
2024-08-26 11:39:35 +0300

    remove v2tov7 prefix

--- src/v2-to-v7/index.ts
+++ src/v2-to-v7/index.ts
@@ -6,6 +6,6 @@ import {buildConfigFromFlashvars} from './utils/flashvars-handler';
   thumbEmbed: V2PlayerThumbEmbed
 };
 
-(window as any).v2tov7_buildConfigFromFlashvars = buildConfigFromFlashvars;
+(window as any).__buildConfigFromFlashvars = buildConfigFromFlashvars;
 
 export {v2PlayerEmbed, V2PlayerThumbEmbed, buildConfigFromFlashvars};
