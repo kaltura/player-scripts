7193e0d0b3f34b5bec5d524bc286704006b5e865
Moshe Maor <moshe.maor@kaltura.com>
2024-09-17 08:37:15 +0300

    Update index.ts

--- src/v2-to-v7/index.ts
+++ src/v2-to-v7/index.ts
@@ -1,6 +1,7 @@
 import {v2PlayerEmbed, V2PlayerThumbEmbed} from './embeds-converter';
 import {buildV7Config} from './utils/flashvars-handler';
 
+
 (window as any).kWidget = {
   embed: v2PlayerEmbed,
   thumbEmbed: V2PlayerThumbEmbed
